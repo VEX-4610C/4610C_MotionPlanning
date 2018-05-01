8735ce5e0e5641efbc735f4e39ed1c0862f914ab
AW <roboFiddle@users.noreply.github.com>
2018-04-30 23:29:02 -0400

    Class Structures for Trajectory (+segment +pair) and Path

--- xStructures/Path.h
+++ xStructures/Path.h
@@ -5,9 +5,23 @@
 #ifndef CUSTOMSPLINES_PATH_H
 #define CUSTOMSPLINES_PATH_H
 
+#include <string>
+#include "Trajectory.cpp"
 
 class Path {
-
+protected:
+    Trajectory::Pair trajectories;
+    std::string name;
+    bool go_left;
+public:
+    explicit Path();
+    std::string getName;
+    void goLeft();
+    void goRight();
+    Trajectory getLeftWheelTrajectory();
+    Trajectory getRightWheelTrajectory();
+    Trajectory::Pair getPair();
+    double getEndHeading();
 };
 
 
--- xStructures/Trajectory.h
+++ xStructures/Trajectory.h
@@ -5,9 +5,41 @@
 #ifndef CUSTOMSPLINES_TRAJECTORY_H
 #define CUSTOMSPLINES_TRAJECTORY_H
 
+#include <string>
+#include <vector>
 
 class Trajectory {
-
+public:
+    class Pair {
+    public:
+        Trajectory left;
+        Trajectory right;
+        Pair(Trajectory l, Trajectory r);
+    };
+    class Segment {
+    public:
+        double pos, vel, acc, jerk, heading, dt, x, y;
+        Segment();
+        Segment(double pos, double vel, double acc, double jerk,
+                double heading, double dt, double x, double y);
+        explicit  Segment(Segment to_copy);
+        std::string toString();
+    };
+    std::vector<Segment> segments;
+    bool inverted_y = 0;
+    explicit Trajectory(int length);
+    explicit Trajectory(std::vector<Segment> segments);
+    void setInvertedY(bool setting);
+    int getNumSegments();
+    Segment getSegment(int index);
+    void setSegment(int index, Segment seg);
+    void scale(double scale_factor);
+    void append(Trajectory to_append);
+    Trajectory copy();
+    std::vector<Segment> copySegments(std::vector<Segment>);
+    std::string toString();
+    std::string toStringProfile();
+    std::string toStringEuclidean();
 };
 
 
